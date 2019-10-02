b8126855457f2eb1580c2dcb47a143ba48457374
Phyllis Kan <pyk9q7@gmail.com>
2019-10-02 17:37:51 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,8 @@
-class Project 
+class Project
   attr_reader :title, :backers
 
   def initialize(title)
-    @title = title 
+    @title = title
     @backers = []
   end
 
@@ -10,5 +10,5 @@ class Project
     backed_projects << backer
     backer.backed_projects << self
   end
-  
+
 end
